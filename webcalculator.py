9b82b4d19faef571d7b455b9e9401eddd2944574
David Hall <david63025@gmail.com>
2016-03-19 22:57:49 -0500

    using stylesheet.css

--- webcalculator.py
+++ webcalculator.py
@@ -14,93 +14,31 @@
 
 __version__ = "0.1.0"
 __author__ = "David Hall"
-__last_update__ = "13 March 2016"
+__last_update__ = "19 March 2016"
 
+import os.path
 import cherrypy
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print(current_dir)
 
 class WebCalculator(object):
     def _header(self):
         return """
         <!DOCTYPE html>
         <html>
-        <head> </head>"""
-
-    def _styles(self):
-        return """
-        <style>
-        body {
-			margin: 0;
-		}
-	
-		ul {
-		    list-style-type: none;
-		    margin: 0;
-		    padding: 0;
-		    width: 20%;
-		    background-color: #f1f1f1;
-		    position: fixed;
-		    height: 100%;
-		    overflow: auto;
-		}
-		
-		li a {
-		    display: block;
-		    color: #000;
-		    padding: 8px 0 8px 16px;
-		    text-decoration: none;
-		}
-		
-		li a.active {
-		    background-color: #4CAF50;
-		    color: white;
-		}
-		
-		li a:hover:not(.active) {
-		    background-color: #555;
-		    color: white;
-		}
-
-        input[type=text] {
-            width: 100%;
-            box-sizing: border-box;
-            padding: 10px 10px;
-            margin: 10px 10px;
-            border: 1px solid #cccccc;
-            border-radius: 8px;
-            font-size: 18px;
-            text-align: center;
-		}
-        .button {
-            width: 100%;
-            box-sizing: border-box;
-            background-color: #4caf50;
-            color: white;
-            padding: 10px 10px;
-            margin: 10px 10px;
-            border: none;
-            text-align: center;
-            text-decoration: none;
-            font-size: 18px;
-            border-radius: 8px;
-		}
-        div {
-			margin-left: 25%;
-            width: 25%;
-            border-radius: 5px;
-            background-color: #f2f2f2;
-            padding: 40px;
-		}
-        h2 {
-            text-align: left;
-        }
-        </style>"""
+        <head>
+            <title>Web Calculators</title>
+            <title>Web Calculators</title>
+            <link rel="stylesheet" type="text/css" href="webcalculator.css">
+        </head>"""
 
     def _body(self, temperature, calc):
-        if calc.endswith('C'):
+        if calc.endswith('c'):
             result = self._f2c(temperature)
             units1 = 'F'
             units2 = 'C'
-        elif calc.endswith('F'):
+        elif calc.endswith('f'):
             result = self._c2f(temperature)
             units1 = 'C'
             units2 = 'F'
@@ -111,20 +49,30 @@ class WebCalculator(object):
         return """
         <body>
         <ul>
-			<li><a class="active" href="#converters">Units Converters</a></li>
-			<li><a href="#eecalc">Electronics Calculators</a></li>
+            <li><a class="active" href="#converters">Units Converters</a></li>
+            <li><a href="#eecalc">Electronics Calculators</a></li>
             <li><a href="#simplecalc">Simple Calculator</a></li>
             <li><a href="#moon">Moon Almanac</a></li>
         </ul>
         <div>
-	        <form method="get" action="calculate">
-	        <h2>Enter Temperature: </h2><br>
-	        <input type="text" name="temperature" value= %.1f /> <br>
-	        <button class="button" type="submit" name="calc" value="F2C">&#176;F &#8680 &#176;C</button>
-	        <button class="button" type="submit" name="calc" value="C2F">&#176;C &#8680 &#176;F</button>
-	        <br>
-	        <h2>%.1f &#176;%s is %.1f &#176;%s</h2>
-        </form> </div> </body>""" % (temperature, temperature, units1, result, units2) 
+            <form method="get" action="calculate">
+            <h2>Enter Temperature: </h2><br>
+            <input type="text" name="temperature" value= %.1f /> <br>
+            <button class="button" type="submit" name="calc" value="f2c">&#176;F &#8680 &#176;C</button>
+            <button class="button" type="submit" name="calc" value="c2f">&#176;C &#8680 &#176;F</button>
+            <br>
+            <h2>%.1f &#176;%s is %.1f &#176;%s</h2>
+        </form> </div>
+        <div>
+            <form method="get" action="calculate">
+            <h2>Enter Distance: </h2><br>
+            <input type="text" name="temperature" value= %.1f /> <br>
+            <button class="button" type="submit" name="calc" value="mi2km">Miles &#8680 Kilometers</button>
+            <button class="button" type="submit" name="calc" value="km2mi">Kilometers &#8680 Miles</button>
+            <br>
+            <h2>%.1f &#176;%s is %.1f &#176;%s</h2>
+        </form> </div> </body>""" % (temperature, temperature, units1, result, units2,
+                                     temperature, temperature, units1, result, units2) 
 
     def _footer(self):
         return """
@@ -135,18 +83,29 @@ class WebCalculator(object):
 
     def _f2c(self, temp):
         return 5.0 * (temp - 32.0) / 9.0
+    
+    def _mi2km(self, dist):
+        return 1.609344 * dist
+
+    def _km2mi(self, dist):
+        return dist / 1.609344
 
     @cherrypy.expose
     def index(self):
-        return (self._header() + self._styles() +
-                self._body(40.0, 'C2F') + self._footer())
+        return (self._header() + self._body(40.0, 'C2F') + self._footer())
 
     @cherrypy.expose
     def calculate(self, calc, temperature):
-        return (self._header() + self._styles() +
-                self._body(float(temperature), calc) + self._footer())
+        return (self._header() + self._body(float(temperature), calc) + self._footer())
 
 # end of class WebCalculator
 
 if __name__ == '__main__':
-    cherrypy.quickstart(WebCalculator())
+    conf = {
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.root': current_dir,
+            'tools.staticdir.dir': current_dir
+        }
+    }
+    cherrypy.quickstart(WebCalculator(), '/', conf)
